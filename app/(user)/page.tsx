5990ea5448b251abe6fc94cfb8033c697ea690f1
unknown <ademayowaadeniyi@gmail.com>
2023-01-09 15:48:59 +0100

    updates

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,22 +1,48 @@
 import { previewData } from 'next/headers';
 import { groq } from 'next-sanity';
+import { client } from '../../lib/sanity.client';
+import PreviewSuspense from '../../components/PreviewSuspense';
+import PreviewBlogList from '../../components/PreviewBlogList';
+import BlogList from '../../components/BlogList';
+
+// export default async function HomePage() {
+//   if (previewData()) {
+//     return (
+//       <PreviewSuspense fallback="Loading...">
+//         <PreviewDocumentsCount />
+//       </PreviewSuspense>
+//     )
+//   }
+
+//   const data = await client.fetch(query)
+//   return <DocumentsCount data={data} />
+// }
 
 const query = groq`
-  *[_type=='post'] {
+  *[_type=='post'] { 
     ...,
     author->,
     categories[]->
   } | order(_createdAt desc)
 `;
 
-export default function HomePage() {
+export default async function HomePage() {
   if (previewData()) {
-    return <div>Preview Mode</div>;
+    return (
+      <PreviewSuspense
+        fallback={
+          <div role='status'>
+            <p className='text-center animate-pulse'>Loading preview data...</p>
+          </div>
+        }
+      >
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    );
   }
 
-  return (
-    <div>
-      <h2>Not in preview mode</h2>
-    </div>
-  );
+  const posts = await client.fetch(query);
+  // console.log(posts);
+
+  return <BlogList posts={posts} />;
 }
---
+++ components/BlogList.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  posts: Post[];
+};
+
+function BlogList({ posts }: Props) {
+  return (
+    <div>
+      <h2>BlogList</h2>
+    </div>
+  );
+}
+
+export default BlogList;
