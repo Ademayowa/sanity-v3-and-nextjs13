7e5d0a663f4b43efcb8c29364bc268f70fca053f
unknown <ademayowaadeniyi@gmail.com>
2023-01-10 10:10:30 +0100

    updates

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -10,19 +10,21 @@ function BlogList({ posts }: Props) {
     <div>
       <hr className='border text-red-300' />
 
-      <div>
-        {posts.map((post) => (
-          <div key={post._id}>
-            <div>
-              <Image
-                src={urlFor(post.mainImage).url()}
-                alt={post.author.name}
-                className='object-cover object-left lg:object-center'
-                fill
-              />
+      <div className='flex flex-col group'>
+        <div className='relative w-50 h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out '>
+          {posts.map((post) => (
+            <div key={post._id}>
+              <div>
+                <Image
+                  src={urlFor(post.mainImage).url()}
+                  alt={post.author.name}
+                  className='object-cover object-left lg:object-center'
+                  fill
+                />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
